23d0ed0e9218f49b203cef08499df3ba22209afb
Suraj Kancharla <surajk@quilt.ai>
2021-08-25 10:40:22 +0530

    Added md for Carousel

--- src/components/carousel-section/index.tsx
+++ src/components/carousel-section/index.tsx
@@ -3,6 +3,7 @@ import { tw } from 'twind/css';
 import Carousel from 'react-multi-carousel';
 import { DotProps } from 'react-multi-carousel/lib/types';
 import 'react-multi-carousel/lib/styles.css';
+import {attributes} from '../../../content/pages/benefits.md'
 
 let CardsArray = [
   {
@@ -66,66 +67,71 @@ const CustomDot = ({ onClick, active }: DotProps) => {
   );
 };
 
-const CarouselSection = () => (
-  <section className={tw(`mx-auto max-w-6xl py-20 items-center`)}>
-    <h3 className={tw(`mb-12 font-sans font-medium text-xl md:text-xl lg:text-3xl text-left`)}>
-      Here are some of the benefits of using Sphere:
-    </h3>
-    <div
-      style={{
-        paddingBottom: '30px',
-      }}
-    >
-      <Carousel
-        swipeable={false}
-        draggable={false}
-        showDots
-        renderDotsOutside={true}
-        customDot={<CustomDot />}
-        responsive={responsive}
-        ssr={true} // means to render carousel on server-side.
-        infinite={true}
-        autoPlaySpeed={1000}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-container"
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-        arrows={false}
+const CarouselSection = () => {
+  let { benefits_list } = attributes
+  console.log(benefits_list)
+  return (
+
+    <section className={tw(`mx-auto max-w-6xl py-20 items-center`)}>
+      <h3 className={tw(`mb-12 font-sans font-medium text-xl md:text-xl lg:text-3xl text-left`)}>
+        Here are some of the benefits of using Sphere:
+      </h3>
+      <div
+        style={{
+          paddingBottom: '30px',
+        }}
       >
-        {CardsArray.map((card, idx) => (
-          <>
-            <div
-              id={card.id}
-              className={tw(`w-full py-12 relative flex items-center bg-gray-100 rounded-lg`)}
-              key={idx}
-            >
-              <div className={tw(`w-full lg:w-1/2`)}>
-                <div className={tw(`max-w-md mx-auto`)}>
-                  <h3 className={tw(`font-sans font-bold text-xl md:text-xl lg:text-3xl text-left text-purple-700`)}>
-                    {card.title}
-                  </h3>
-                  <p className={tw(`mt-5 mr-16 text-center text-2xl lg:text-xl text-left`)}>{card.body}</p>
+        <Carousel
+          swipeable={false}
+          draggable={false}
+          showDots
+          renderDotsOutside={true}
+          customDot={<CustomDot />}
+          responsive={responsive}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          autoPlaySpeed={1000}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+          arrows={false}
+        >
+          {benefits_list.map((card, idx) => (
+            <>
+              <div
+                id={card.id}
+                className={tw(`w-full py-12 relative flex items-center bg-gray-100 rounded-lg`)}
+                key={idx}
+              >
+                <div className={tw(`w-full lg:w-1/2`)}>
+                  <div className={tw(`max-w-md mx-auto`)}>
+                    <h3 className={tw(`font-sans font-bold text-xl md:text-xl lg:text-3xl text-left text-purple-700`)}>
+                      {card.card_heading}
+                    </h3>
+                    <p className={tw(`mt-5 mr-16 text-center text-2xl lg:text-xl text-left`)}>{card.card_body}</p>
+                  </div>
+                </div>
+                <div className={tw(`w-full lg:w-1/2 mx-16`)}>
+                  <div className={tw(`lg:mb-12 lg:mb-0 pb-12 lg:pb-0 mt-16 lg:mt-0 mx-6 lg:mx-0`)}>
+                    <img
+                      src={card.card_image}
+                      className={tw(`h-full w-full object-cover overflow-hidden rounded`)}
+                      width={400}
+                      height={300}
+                    />
+                  </div>
                 </div>
               </div>
-              <div className={tw(`w-full lg:w-1/2 mx-16`)}>
-                <div className={tw(`lg:mb-12 lg:mb-0 pb-12 lg:pb-0 mt-16 lg:mt-0 mx-6 lg:mx-0`)}>
-                  <img
-                    src={card.image}
-                    className={tw(`h-full w-full object-cover overflow-hidden rounded`)}
-                    width={400}
-                    height={300}
-                  />
-                </div>
-              </div>
-            </div>
-          </>
-        ))}
-      </Carousel>
-    </div>
-  </section>
-);
+            </>
+          ))}
+        </Carousel>
+      </div>
+    </section>
+  );
+}
 
 export default CarouselSection;
 
